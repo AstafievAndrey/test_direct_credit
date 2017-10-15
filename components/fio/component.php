88ff857da53d90168b7d46c3129c5c9ec77af944
Андрей Астафьев <carrats@yandex.ru>
2017-10-15 11:27:18 +0300

    добавление

--- components/fio/component.php
+++ components/fio/component.php
@@ -3,70 +3,74 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 use Bitrix\Main,
     Bitrix\Main\Loader;
 
+
+
 if(isset($_POST['ajax']) || isset($_GET['ajax'])){
     $request = json_decode(file_get_contents("php://input"));
     
     if (!Loader::includeModule('iblock'))
 	return;
-    $arrFio=[];
-    $arSelect = [
-        "ID", "NAME", "DATE_ACTIVE_FROM",
-        "PROPERTY_SURNAME","PROPERTY_NAME",
-        "PROPERTY_LASTNAME"
-    ];
-    $arFilter = [
-        "IBLOCK_ID"=>IntVal(1), 
-        "ACTIVE_DATE"=>"Y", 
-        "ACTIVE"=>"Y"
-    ];
-    $arNavStartParams = [
-        "nPageSize"=>(int)$request->nPageSize,
-        "iNumPage"=>(int)$request->iNumPage
-    ];
     
-    $res = CIBlockElement::GetList([], $arFilter, false, $arNavStartParams, $arSelect);
-    $arrFio["totalItems"]=intval($res->SelectedRowsCount());
-    while($ob = $res->GetNextElement())
-    {
-        $arFields = $ob->GetFields();  
-        $arrFio["fio"][]=array(
-            "ID"=>$arFields["ID"],
-            "PROPERTY"=>array(
-                "SURNAME"=>$arFields["PROPERTY_SURNAME_VALUE"],
-                "NAME"=>$arFields["PROPERTY_NAME_VALUE"],
-                "LASTNAME"=>$arFields["PROPERTY_LASTNAME_VALUE"],
-                "DISABLED"=>true,
-                "LOADSAVEFIO"=>false,
-            )
-        );
+    $action = isset($_GET['action']) ? $_GET['action'] : null;
+    
+    switch ($action){
+        case null:
+            header("HTTP/1.0 404 Not Found");
+            break;
+        case "list":
+            $res = listFio($request);
+            break;
+        case "addFio":
+            $res = addFio($request);
+            break;
     }
+
     $APPLICATION->RestartBuffer();
     header('Content-Type: application/json');
-    echo Main\Web\Json::encode($arrFio);
+    echo Main\Web\Json::encode($res);
     die();
 }else{
     $this->IncludeComponentTemplate();
 }
 
+function addFio($request){
+    $el  = new CIBlockElement;
+    $arLoadProductArray = Array(
+      "IBLOCK_ID"       => 1,
+      "PROPERTY_VALUES" => array(
+           "SURNAME"    => trim(iconv("utf-8", "windows-1251", $request->surname)),
+           "FIRSTNAME"       => trim(iconv("utf-8", "windows-1251", $request->name)),
+           "LASTNAME"   => trim(iconv("utf-8", "windows-1251", $request->lastname)),
+        ),
+      "NAME"            => "Элемент",
+    );
+    $PRODUCT_ID = $el->Add($arLoadProductArray);
+    if($PRODUCT_ID){
+        $res = ["result"=>true];
+    }else{
+        $res = ["result"=>false,"result"=>$el->LAST_ERROR];
+    }
+    return  $res;
+}
 
-
-
-//$APPLICATION->AddHeadScript($this->getPath()."/js/fio.js");
-
-
-//$el = new CIBlockElement;
-//
-//$arLoadProductArray = Array(
-//  "IBLOCK_ID"      => 1,
-//  "PROPERTY_VALUES"=> array(
-//       "SURNAME" => "Сидоров",
-//       "NAME" => "Иван",
-//       "LASTNAME" => "Иванович",
-//    ),
-//  "NAME"           => "Элемент",
-//);
-//
-//if($PRODUCT_ID = $el->Add($arLoadProductArray))
-//  echo "New ID: ".$PRODUCT_ID;
-//else
-//  echo "Error: ".$el->LAST_ERROR;
+function listFio($request){
+    $arrFio=[];
+    $arSelect = ["ID", "NAME","PROPERTY_SURNAME","PROPERTY_FIRSTNAME","PROPERTY_LASTNAME"];
+    $arFilter = ["IBLOCK_ID"=>IntVal(1), "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y"];
+    $arNavStartParams = ["nPageSize"=>(int)$request->nPageSize, "iNumPage"=>(int)$request->iNumPage];
+    $res = CIBlockElement::GetList([], $arFilter, false, $arNavStartParams, $arSelect);
+    $arrFio["totalItems"]=intval($res->SelectedRowsCount());
+    while($ob = $res->GetNextElement()){
+        $arFields = $ob->GetFields();  
+        $arrFio["fio"][]=array(
+            "ID"=>$arFields["ID"],
+            "PROPERTY"=>array(
+                "SURNAME"=>$arFields["PROPERTY_SURNAME_VALUE"],
+                "FIRSTNAME"=>$arFields["PROPERTY_FIRSTNAME_VALUE"],
+                "LASTNAME"=>$arFields["PROPERTY_LASTNAME_VALUE"],
+                "DISABLED"=>true,"LOADSAVEFIO"=>false,
+            )
+        );
+    }
+    return $arrFio;
+}
--- components/fio/templates/.default/template.php
+++ components/fio/templates/.default/template.php
@@ -1,100 +1,120 @@
-<div class="container" ng-app="fio">
-    <div class="row" ng-controller="FioListController">
-        <div class="col-sm-12">
-            <table class="table" style="min-height: 275px">
-                <thead class="thead-inverse">
-                    <tr>
-                        <th>#</th>
-                        <th>Фамилия</th>
-                        <th>Имя</th>
-                        <th>Отчество</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr ng-repeat="(key, value) in fio">
-                        <td>{{value.ID}}</th>
-                        <td><input ng-disabled="value.PROPERTY.DISABLED || value.PROPERTY.LOADSAVEFIO" 
-                                   ng-model="value.PROPERTY.SURNAME" ng-pattern="regex" 
-                                   class="table-input"></td>
-                        <td><input ng-disabled="value.PROPERTY.DISABLED || value.PROPERTY.LOADSAVEFIO" 
-                                   ng-model="value.PROPERTY.NAME" ng-pattern="regex"
-                                   class="table-input"></td>
-                        <td><input ng-disabled="value.PROPERTY.DISABLED || value.PROPERTY.LOADSAVEFIO" 
-                                   ng-model="value.PROPERTY.LASTNAME" ng-pattern="regex"
-                                   class="table-input"></td>
-                        <td>
-                            <span ng-show="value.PROPERTY.LOADSAVEFIO"
-                                  class="glyphicon glyphicon-refresh"></span>
-                            <span ng-show="value.PROPERTY.DISABLED" ng-click="update(key)" 
-                                  class="glyphicon glyphicon-pencil"></span>
-                            <span ng-hide="value.PROPERTY.DISABLED || value.PROPERTY.LOADSAVEFIO" ng-click="update(key)"
-                                  class="glyphicon glyphicon-ok"></span>
-                        </td>
-                    </tr>
-                    <tr ng-if=" (fio.data.length == 0) && (!loading)">
-                        <td colspan="5" align="center">
-                            Нет данных
-                        </td>
-                    </tr>
-                    <tr ng-if="loading">
-                        <td colspan="5" align="center">
-                            <div class="loader">Loading...</div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+<div class="container-fluid" ng-app="fio">
+    <div class="container" ng-controller="FioListController">
+        <div class="row">
+            <div class="col-sm-3">
+                <button ng-click="showAddForm = ((showAddForm) ? false : true) " class="btn btn-primary">
+                    {{(showAddForm) ? 'Скрыть' : 'Добавить'}}
+                </button>
+            </div>
         </div>
-        <div class="col-xs-6 ">
-            <pagination ng-model="pagination.iNumPage"
-                ng-change="pageChanged()"
-                total-items="pagination.totalItems"
-                max-size="pagination.maxSize"  
-                items-per-page="pagination.nPageSize"
-                previous-text="&lsaquo;" next-text="&rsaquo;" 
-                first-text="&laquo;" last-text="&raquo;"
-                boundary-links="true">
-            </pagination>
-        </div>    
-        <div class="col-xs-6 col-sm-1 col-sm-offset-5">
-            <select ng-change="nPageSizeChanged()" ng-model="pagination.nPageSize" class="form-control" 
-                    style="margin:20px 0px">
-                <option value="1">1</option>
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="50">50</option>
-            </select>
+        <div class="row" ng-hide="showAddForm">
+            <div class="col-sm-12">
+                <table class="table" style="min-height: 275px">
+                    <thead class="thead-inverse">
+                        <tr>
+                            <th>#</th>
+                            <th>Фамилия</th>
+                            <th>Имя</th>
+                            <th>Отчество</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr ng-repeat="(key, value) in fio">
+                            <td>{{value.ID}}</th>
+                            <td><input ng-disabled="value.PROPERTY.DISABLED || value.PROPERTY.LOADSAVEFIO" 
+                                       ng-model="value.PROPERTY.SURNAME" ng-pattern="/^[А-Яа-я]+$/" 
+                                       class="table-input"></td>
+                            <td><input ng-disabled="value.PROPERTY.DISABLED || value.PROPERTY.LOADSAVEFIO" 
+                                       ng-model="value.PROPERTY.FIRSTNAME" ng-pattern="/^[А-Яа-я]+$/"
+                                       class="table-input"></td>
+                            <td><input ng-disabled="value.PROPERTY.DISABLED || value.PROPERTY.LOADSAVEFIO" 
+                                       ng-model="value.PROPERTY.LASTNAME" ng-pattern="/^[А-Яа-я]+$/"
+                                       class="table-input"></td>
+                            <td>
+                                <span ng-show="value.PROPERTY.LOADSAVEFIO"
+                                      class="glyphicon glyphicon-refresh"></span>
+                                <span ng-show="value.PROPERTY.DISABLED" ng-click="update(key)" 
+                                      class="glyphicon glyphicon-pencil"></span>
+                                <span ng-hide="value.PROPERTY.DISABLED || value.PROPERTY.LOADSAVEFIO" 
+                                      ng-click="update(key,value)"
+                                      class="glyphicon glyphicon-ok"></span>
+                            </td>
+                        </tr>
+                        <tr ng-if=" (fio.length == 0) && (!loading)">
+                            <td colspan="5" align="center">
+                                Нет данных
+                            </td>
+                        </tr>
+                        <tr ng-if="message.show">
+                            <td colspan="5" align="center">
+                                {{message.text}}
+                            </td>
+                        </tr>
+                        <tr ng-if="loading">
+                            <td colspan="5" align="center">
+                                <div class="loader">Loading...</div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-xs-6 ">
+                <pagination ng-model="pagination.iNumPage"
+                    ng-change="pageChanged()"
+                    total-items="pagination.totalItems"
+                    max-size="pagination.maxSize"  
+                    items-per-page="pagination.nPageSize"
+                    previous-text="&lsaquo;" next-text="&rsaquo;" 
+                    first-text="&laquo;" last-text="&raquo;"
+                    boundary-links="true">
+                </pagination>
+            </div>    
+            <div class="col-xs-6 col-sm-1 col-sm-offset-5">
+                <select ng-change="nPageSizeChanged()" ng-model="pagination.nPageSize" class="form-control" 
+                        style="margin:20px 0px">
+                    <option value="1">1</option>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option value="50">50</option>
+                </select>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-offset-3 col-sm-6">
-            <form novalidate name="addFioForm" id="addFio" class="form-horizontal">
-                <div class="form-group">
-                    <label class="col-sm-3">Фамилия</label>
-                    <div class="col-sm-9">
-                        <input ng-model="addFio.surname" 
-                               name="surname" pattern="^[А-Яа-я]+$"
-                               class="form-control" required>
+        <div class="row" ng-show="showAddForm">
+            <div class="col-sm-offset-3 col-sm-6">
+                <form novalidate ng-submit="sendFio(addFioForm)" name="addFioForm" id="addFio" class="form-horizontal">
+                    <div class="form-group">
+                        <label class="col-sm-3">Фамилия</label>
+                        <div class="col-sm-9">
+                            <input ng-model="addFio.surname" pattern="^[А-Яа-я]+$"
+                                   class="form-control" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3">Имя</label>
-                    <div class="col-sm-9">
-                        <input ng-model="addFio.name" 
-                               name="name" pattern="^[А-Яа-я]+$"
-                               class="form-control" required>
+                    <div class="form-group">
+                        <label class="col-sm-3">Имя</label>
+                        <div class="col-sm-9">
+                            <input ng-model="addFio.firstname" 
+                                   class="form-control" required>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-3">Отчество</label>
-                    <div class="col-sm-9">
-                        <input ng-model="addFio.lastname" 
-                               name="lastname" pattern="^[А-Яа-я]+$"
-                               class="form-control" required>
+                    <div class="form-group">
+                        <label class="col-sm-3">Отчество</label>
+                        <div class="col-sm-9">
+                            <input ng-model="addFio.lastname" pattern="^[А-Яа-я]+$"
+                                   class="form-control" required>
+                        </div>
                     </div>
-                </div>
-            </form>
+                    <div class="form-group">
+                        <label class="col-sm-3">
+                            <button ng-disabled="addFioForm.$invalid"
+                                type="submit" class="btn btn-primary">
+                                Добавить
+                            </button>
+                        </label>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
@@ -102,29 +122,65 @@
 var fioApp = angular.module('fio', ['ui.bootstrap']);
 fioApp.controller('FioListController', function FioListController($http,$scope,$timeout) {
     
-    $scope.addFio = {surname : '', name : '', lastname : ''}
-    $scope.regex = '/^[А-Яа-я]+$/';
-    
+    $scope.showAddForm = false;
+    $scope.addFio = {surname : null, firstname : null, lastname : null};
+    $scope.message = {show:false, text:""};
     $scope.pagination = {
         totalItems : 0,
         iNumPage : 1,
         maxSize : 5,
         nPageSize : '5'
     };
-    
     $scope.fio;
     $scope.loading;
     $scope.url = 'index.php?ajax=1';
     
-    $scope.update = function(key){
+    $scope.clearMessage = function(){
+        $scope.message = {show:false, text:""};
+    }
+    
+    $scope.sendFio = function(form){
+        $http.post($scope.url+"&action=addFio",{
+            surname: $scope.addFio.surname,
+            name: $scope.addFio.firstname,
+            lastname: $scope.addFio.lastname
+        })
+        .then(function(response) {
+            if(response.status === 200 ){
+                $scope.addFio = {surname : null, firstname : null, lastname : null};
+                form.$setPristine();
+                $scope.pageChanged();
+            }else{
+                alert("Что то пошло не так");
+            }
+        },function(){
+            alert("Error action addFio");
+        });
+    };
+    
+    $scope.check = function(value){
+        if( (value.PROPERTY.SURNAME.search(/^[А-Яа-я]+$/) === -1) || 
+            (value.PROPERTY.FIRSTNAME.search(/^[А-Яа-я]+$/) === -1)
+            (value.PROPERTY.LASTNAME.search(/^[А-Яа-я]+$/) === -1)){
+            return false;
+        }
+        return true;
+    }
+    
+    $scope.update = function(key,value){
         if($scope.fio[key].PROPERTY.DISABLED){
             $scope.fio[key].PROPERTY.DISABLED = false;
         }else{
-            $scope.fio[key].PROPERTY.LOADSAVEFIO = true;
-            $timeout(function(){
-                $scope.fio[key].PROPERTY.LOADSAVEFIO = false;
-                $scope.fio[key].PROPERTY.DISABLED = true;
-            },1000);  
+            if($scope.check(value)){
+                $scope.fio[key].PROPERTY.LOADSAVEFIO = true;
+                $timeout(function(){
+                    $scope.fio[key].PROPERTY.LOADSAVEFIO = false;
+                    $scope.fio[key].PROPERTY.DISABLED = true;
+                },1000);  
+            }else{
+                $scope.message = {show:true,text:"Проверьте правильность написания ФИО"};
+                $timeout(function(){$scope.clearMessage()},2000); 
+            }
         }
     }
     
@@ -133,14 +189,10 @@ fioApp.controller('FioListController', function FioListController($http,$scope,$
         $scope.pageChanged();
     }
     
-    $scope.addFio = function(){
-        
-    }
-    
     $scope.pageChanged = function() {
         $scope.loading = true;
         $scope.fio = [];
-        $http.post($scope.url,$scope.pagination).then(function(response) {
+        $http.post($scope.url+"&action=list",$scope.pagination).then(function(response) {
             if(response.status === 200 ){
                  $timeout(function(){
                     $scope.pagination.totalItems = response.data.totalItems;
@@ -151,6 +203,8 @@ fioApp.controller('FioListController', function FioListController($http,$scope,$
                 alert("Что то пошло не так");
                 $scope.loading = false;
             }
+        },function(){
+            alert("Error action list");
         });
     };
     $scope.pageChanged();
